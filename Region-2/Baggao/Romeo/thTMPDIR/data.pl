115d7d6fac4007ee59f49d88689e0b3a6971587a
speleo <henry@hbennett.com>
2017-02-10 17:24:14 +0000

    Baggao cleanup

--- Region-2/Baggao/Romeo/thTMPDIR/data.pl
+++ Region-2/Baggao/Romeo/thTMPDIR/data.pl
@@ -1,21 +1,141 @@
+# COLOR LEGEND
+#  100   40   40 \thfb\char52 \char53 \char53 \mainfont{}\thinspace \thfb\char109 \mainfont{}
+#  100   99   40 \thfb\char52 \char52 \char51 \mainfont{}\thinspace \thfb\char109 \mainfont{}
+#   40  100   40 \thfb\char52 \char51 \char48 \mainfont{}\thinspace \thfb\char109 \mainfont{}
+#   40  100   99 \thfb\char52 \char49 \char56 \mainfont{}\thinspace \thfb\char109 \mainfont{}
+#   40   40  100 \thfb\char52 \char48 \char54 \mainfont{}\thinspace \thfb\char109 \mainfont{}
+#   99   40  100 \thfb\char51 \char57 \char51 \mainfont{}\thinspace \thfb\char109 \mainfont{}
+
+
+
 %SCRAP = (
-	# scrap: lumioSE1
+	# scrap: romeo_cathedralSP1
 	aaaaaaa => {
-		 B => "data.1",
-		 I => "data.1bg",
-		 C => "data.1clip",
-		 E => "data.2",
+		 F => "data.1",
+		 B => "data.2",
+		 I => "data.2bg",
+		 C => "data.2clip",
+		 E => "data.3",
+		 X => "data.4",
+		 P => "data.4bbox",
 		 Y => 1,
 		 V => -1,
-		 S => "470.61 -269.62",
+		 S => "523.68 -883.16",
+	},
+	# scrap: romeoSP3
+	aaaaaab => {
+		 F => "data.5",
+		 B => "data.6",
+		 I => "data.6bg",
+		 C => "data.6clip",
+		 E => "data.7",
+		 X => "data.8",
+		 P => "data.8bbox",
+		 Y => 2,
+		 V => -2,
+		 S => "980.76 -706.64",
+	},
+	# scrap: romeoSP2
+	aaaaaac => {
+		 F => "data.9",
+		 B => "data.10",
+		 I => "data.10bg",
+		 C => "data.10clip",
+		 E => "data.11",
+		 X => "data.12",
+		 P => "data.12bbox",
+		 Y => 2,
+		 V => -2,
+		 S => "2178.25 -1184.43",
+	},
+	# scrap: romeoSP1
+	aaaaaad => {
+		 F => "data.13",
+		 B => "data.14",
+		 I => "data.14bg",
+		 C => "data.14clip",
+		 E => "data.15",
+		 X => "data.16",
+		 P => "data.16bbox",
+		 Y => 2,
+		 V => -2,
+		 S => "1431.11 -4527.59",
+	},
+	# scrap: romeoSP0
+	aaaaaae => {
+		 F => "data.17",
+		 B => "data.18",
+		 I => "data.18bg",
+		 C => "data.18clip",
+		 E => "data.19",
+		 X => "data.20",
+		 P => "data.20bbox",
+		 Y => 2,
+		 V => -1,
+		 S => "955.29 -6708.29",
+	},
+	# scrap: Romeo_21_1SP1
+	aaaaaaf => {
+		 F => "data.21",
+		 B => "data.22",
+		 I => "data.22bg",
+		 C => "data.22clip",
+		 E => "data.23",
+		 X => "data.24",
+		 P => "data.24bbox",
+		 Y => 3,
+		 V => -1,
+		 S => "1358.10 -3626.41",
+	},
+	# scrap: Romeo_1.12SP1
+	aaaaaag => {
+		 F => "data.25",
+		 B => "data.26",
+		 I => "data.26bg",
+		 C => "data.26clip",
+		 E => "data.27",
+		 X => "data.28",
+		 P => "data.28bbox",
+		 Y => 4,
+		 V => -1,
+		 S => "192.42 -7137.39",
+	},
+	# scrap: lumioSP1
+	aaaaaah => {
+		 F => "data.29",
+		 B => "data.30",
+		 I => "data.30bg",
+		 C => "data.30clip",
+		 E => "data.31",
+		 X => "data.32",
+		 P => "data.32bbox",
+		 Y => 5,
+		 V => -1,
+		 S => "1581.60 -3813.17",
 	},
 );
 
 
 %LAYER = (
-	# expanded map: lumioME
+	# expanded map: romeo_cathedralM
 	1 => {
-		N => 'lumioME',
+		N => 'romeo\_cathedralM',
+	},
+	# expanded map: romeo_entranceM
+	2 => {
+		N => 'romeo\_entranceM',
+	},
+	# expanded map: Romeo_21_1M
+	3 => {
+		N => 'Romeo\_21\_1M',
+	},
+	# expanded map: Romeo_1.12M
+	4 => {
+		N => 'Romeo\_1.12M',
+	},
+	# expanded map: lumioM
+	5 => {
+		N => 'lumioM',
 	},
 );
 
