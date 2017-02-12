e7ca40019f8eeffdb35831ea3dc37d2b25aca305
speleo <henry@hbennett.com>
2017-02-12 08:57:21 +0000

    Romeo Google Earth Clean
    geting export map to work in Google Earth

--- Region-2/Baggao/Romeo/thTMPDIR/data.pl
+++ Region-2/Baggao/Romeo/thTMPDIR/data.pl
@@ -22,7 +22,7 @@
 		 V => -1,
 		 S => "523.68 -883.16",
 	},
-	# scrap: romeoSP3
+	# scrap: romeoSP4
 	aaaaaab => {
 		 F => "data.5",
 		 B => "data.6",
@@ -33,9 +33,9 @@
 		 P => "data.8bbox",
 		 Y => 2,
 		 V => -2,
-		 S => "980.76 -706.64",
+		 S => "1324.28 -5466.36",
 	},
-	# scrap: romeoSP2
+	# scrap: romeoSP3
 	aaaaaac => {
 		 F => "data.9",
 		 B => "data.10",
@@ -46,9 +46,9 @@
 		 P => "data.12bbox",
 		 Y => 2,
 		 V => -2,
-		 S => "2178.25 -1184.43",
+		 S => "980.76 -706.64",
 	},
-	# scrap: romeoSP1
+	# scrap: romeoSP2
 	aaaaaad => {
 		 F => "data.13",
 		 B => "data.14",
@@ -59,56 +59,67 @@
 		 P => "data.16bbox",
 		 Y => 2,
 		 V => -2,
-		 S => "1431.11 -4527.59",
+		 S => "2178.25 -1184.43",
 	},
-	# scrap: romeoSP0
+	# scrap: romeoSP1
 	aaaaaae => {
 		 F => "data.17",
 		 B => "data.18",
 		 I => "data.18bg",
 		 C => "data.18clip",
 		 E => "data.19",
-		 X => "data.20",
-		 P => "data.20bbox",
+		 Y => 2,
+		 V => -2,
+		 S => "1497.20 -4413.02",
+	},
+	# scrap: romeoSP0
+	aaaaaaf => {
+		 F => "data.20",
+		 B => "data.21",
+		 I => "data.21bg",
+		 C => "data.21clip",
+		 E => "data.22",
+		 X => "data.23",
+		 P => "data.23bbox",
 		 Y => 2,
 		 V => -1,
 		 S => "955.29 -6708.29",
 	},
 	# scrap: Romeo_21_1SP1
-	aaaaaaf => {
-		 F => "data.21",
-		 B => "data.22",
-		 I => "data.22bg",
-		 C => "data.22clip",
-		 E => "data.23",
-		 X => "data.24",
-		 P => "data.24bbox",
+	aaaaaag => {
+		 F => "data.24",
+		 B => "data.25",
+		 I => "data.25bg",
+		 C => "data.25clip",
+		 E => "data.26",
+		 X => "data.27",
+		 P => "data.27bbox",
 		 Y => 3,
 		 V => -1,
 		 S => "1358.10 -3626.41",
 	},
 	# scrap: Romeo_1.12SP1
-	aaaaaag => {
-		 F => "data.25",
-		 B => "data.26",
-		 I => "data.26bg",
-		 C => "data.26clip",
-		 E => "data.27",
-		 X => "data.28",
-		 P => "data.28bbox",
+	aaaaaah => {
+		 F => "data.28",
+		 B => "data.29",
+		 I => "data.29bg",
+		 C => "data.29clip",
+		 E => "data.30",
+		 X => "data.31",
+		 P => "data.31bbox",
 		 Y => 4,
 		 V => -1,
 		 S => "192.42 -7137.39",
 	},
 	# scrap: lumioSP1
-	aaaaaah => {
-		 F => "data.29",
-		 B => "data.30",
-		 I => "data.30bg",
-		 C => "data.30clip",
-		 E => "data.31",
-		 X => "data.32",
-		 P => "data.32bbox",
+	aaaaaai => {
+		 F => "data.32",
+		 B => "data.33",
+		 I => "data.33bg",
+		 C => "data.33clip",
+		 E => "data.34",
+		 X => "data.35",
+		 P => "data.35bbox",
 		 Y => 5,
 		 V => -1,
 		 S => "1581.60 -3813.17",
